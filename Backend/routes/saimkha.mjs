9704e4cce2f99ccfc0856c3436a1a6b3592d8e29
saimraza321 <146342788+saimraza321@users.noreply.github.com>
2025-11-30 15:45:59 +0500

    Create saimkha.mjs

---
+++ Backend/routes/saimkha.mjs
@@ -0,0 +1,6 @@
+CLOUDINARY_API_sECRET=MbV16Q1Y0ugsf-H22lpuG0cp5Lw
+cloudkaname=dfcrzmjgu
+CLOUDINARY_kEY=668321521445711
+JWT_SECRET="FitLytics123456789"
+EMAIL_USER="raja.yousuf63@gmail.com"
+EMAIL_PASS="qchw yewl miwu dlfc"
