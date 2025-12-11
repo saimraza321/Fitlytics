a272e86cd002cd594bbd771bddd72874faa78041
saimraza321 <146342788+saimraza321@users.noreply.github.com>
2025-12-12 01:07:09 +0500

    Delete Fitlytics directory

--- Fitlytics/Backend/models/DashboardRoutes.mjs
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getDashboardData } from "../controller/dashboardController.mjs";
-
-const dashboardRouter = express.Router();
-
-dashboardRouter.get("/", getDashboardData);
-
-export default dashboardRouter;
