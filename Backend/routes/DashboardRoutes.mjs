c75078a8ce9f365180187b5e2a868e5020e10354
saimraza321 <146342788+saimraza321@users.noreply.github.com>
2025-12-14 18:57:20 +0500

    Delete Backend directory

--- Backend/routes/DashboardRoutes.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getDashboardData } from "../controller/dashboardController.mjs";
-import userController from "../controller/userController.mjs";
-
-const dashboardRouter = express.Router();
-
-dashboardRouter.get("/", userController.authMiddleware, getDashboardData);
-
-export default dashboardRouter;
